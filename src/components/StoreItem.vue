ccf8e1c5d62d53027558989b18698fafb01a16b4
Matthew Bauer <sylk@protonmail.com>
2019-01-01 20:48:50 -0600

    Initial Commit

---
+++ src/components/StoreItem.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1>{{item.name}}</h1>
+    <p>{{item.description}}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'StoreItem',
+}
+</script>
---
+++ src/views/Store.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <StoreItem
+    :v-for="item in store"/>
+  </div>
+</template>
+
+<script>
+import StoreItem from '@/components/StoreItem.vue'
+
+export default {
+  name: 'store',
+  components: {
+    StoreItem
+  },
+  data() {
+    return {
+        store: [
+            {item: 'Sock', description: 'Just a sock you know?'},
+            {item: 'Yam', description: 'Just a yam you know?'}
+        ]
+    }
+  }
+}
+</script>
